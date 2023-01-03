f43c1ed16d7182874ba9a6761476a19bb57df915
Luca Colomba <lc.colomba@gmail.com>
2023-01-03 13:33:54 +0000

    updated cache

--- cache.py
+++ cache.py
@@ -2,7 +2,7 @@ import os
 from collections import defaultdict
 
 from bridge_clustering.functions import compute_neighbors, determine_bridges
-from utils import read_arff
+from utils import read_arff, read_dataset
 
 class DatasetBridgeCache:
     def __init__(self, path) -> None:
@@ -12,32 +12,32 @@ class DatasetBridgeCache:
         self.bridge_cache = defaultdict(dict)
         self.neighbor_cache = defaultdict(dict)
 
-    def append_arff(self, dataset):
-        if not dataset.endswith('.arff'):
-            dataset = dataset + '.arff'
+    def append_arff(self, dataset, suffix='arff'):
+        if not dataset.endswith(f'.{suffix}'):
+            dataset = dataset + f'.{suffix}'
         return dataset
 
-    def get_data(self, dataset):
-        dataset = self.append_arff(dataset)
+    def get_data(self, dataset, suffix='arff'):
+        dataset = self.append_arff(dataset, suffix)
         if not dataset in self.data_cache:
-            X, cluster_labels = read_arff(os.path.join(self.path, dataset))
+            X, cluster_labels = read_dataset(os.path.join(self.path, dataset))
             self.data_cache[dataset] = (X, cluster_labels)
 
         return self.data_cache[dataset]
 
-    def get_neighbor(self, dataset, k):
-        dataset = self.append_arff(dataset)
+    def get_neighbor(self, dataset, k, suffix='arff'):
+        dataset = self.append_arff(dataset, suffix)
         if not dataset in self.neighbor_cache or not k in self.neighbor_cache[dataset]:
-            X, _ = self.get_data(dataset)
+            X, _ = self.get_data(dataset, suffix)
             local_distances, local_indices = compute_neighbors(X, k)
             self.neighbor_cache[dataset][k] = (local_distances, local_indices)
         return self.neighbor_cache[dataset][k]
 
-    def get_bridge(self, dataset, k):
-        dataset = self.append_arff(dataset)
+    def get_bridge(self, dataset, k, suffix='arff'):
+        dataset = self.append_arff(dataset, suffix)
         if not dataset in self.bridge_cache or not k in self.bridge_cache[dataset]:
-            _, cluster_labels = self.get_data(dataset)
-            _, local_indices = self.get_neighbor(dataset, k)
+            _, cluster_labels = self.get_data(dataset, suffix)
+            _, local_indices = self.get_neighbor(dataset, k, suffix)
 
             is_bridge = determine_bridges(cluster_labels, local_indices) == 0
             self.bridge_cache[dataset][k] = is_bridge
